b733ef57ef8d958d62e6b82dbd6bcaa307256391
Y-Fujikawa <fujikawa0@gmail.com>
2015-11-14 18:30:34 +0900

    Use compass and gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp      = require('gulp');
 var sass      = require('gulp-ruby-sass');
+var plumber   = require('gulp-plumber');
 
 gulp.task('sass', function() {
-  return sass('_src/sass/')
-    .pipe(plumber)
+  return sass('_src/sass/style.scss', { style: 'expanded', compass: true })
+    .pipe(plumber())
     .pipe(gulp.dest('www/css/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('_src/sass/**/*.scss', ['sass']);
 });
-
