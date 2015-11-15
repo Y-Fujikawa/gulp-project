a872406a8e5f530c08f0e6a4b427f6e5beb00c24
Y-Fujikawa <fujikawa0@gmail.com>
2015-11-15 10:33:18 +0900

    Add gulp-connect and Create task gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp      = require('gulp');
 var sass      = require('gulp-ruby-sass');
 var plumber   = require('gulp-plumber');
+var connect   = require('gulp-connect');
 
 gulp.task('sass', function() {
   return sass('_src/sass/style.scss', { style: 'expanded', compass: true })
@@ -11,3 +12,13 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
   gulp.watch('_src/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './www/',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['connect', 'watch']);
+
